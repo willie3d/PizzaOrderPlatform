c14d6e3efa700b95e06c1c2e78f67efaa552df70
willie3d <www.willdoyle10@gmail.com>
2021-11-30 12:14:35 -0500

    Pizza Project
    baseline

---
+++ pizza/src/main/java/com/example/pizza/HelloApplication.java
@@ -0,0 +1,39 @@
+package com.example.pizza;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Priority;
+
+
+
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ pizza/src/main/java/com/example/pizza/HelloController.java
@@ -0,0 +1,143 @@
+package com.example.pizza;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.control.ListView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloController {
+    ObservableList<String> pizzaSizeList = FXCollections.observableArrayList("Small","Medium","Large");
+    ObservableList<String> sodaSizeList = FXCollections.observableArrayList("Can","Bottle","2-Liter");
+    List<HBoxCell> list = new ArrayList<>();
+    ObservableList<HBoxCell> myObservableList = FXCollections.observableList(list);
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private ComboBox cheeseSize;
+    @FXML
+    private ComboBox pepSize;
+    @FXML
+    private ComboBox supSize;
+    @FXML
+    private ComboBox pepsiSize;
+    @FXML
+    private ComboBox cokeSize;
+    @FXML
+    private ComboBox dewSize;
+    @FXML
+    private TextField cheeseQuant;
+    @FXML
+    private TextField pepQuant;
+    @FXML
+    private TextField supQuant;
+    @FXML
+    private TextField pepsiQuant;
+    @FXML
+    private TextField cokeQuant;
+    @FXML
+    private TextField dewQuant;
+    @FXML
+    private Button cheeseAdd;
+    @FXML
+    private Button pepAdd;
+    @FXML
+    private Button supAdd;
+    @FXML
+    private Button pepsiAdd;
+    @FXML
+    private Button cokeAdd;
+    @FXML
+    private Button dewAdd;
+    @FXML
+    BorderPane layout = new BorderPane();
+    @FXML
+    ListView<HBoxCell> listView = new ListView<HBoxCell>();
+
+
+
+
+
+    public static class HBoxCell extends HBox {
+        Label label = new Label();
+        Button button = new Button();
+
+        HBoxCell(String labelText, String buttonText) {
+            super();
+            int cost = 0;
+            label.setText(labelText);
+            label.setMaxWidth(Double.MAX_VALUE);
+            HBox.setHgrow(label, Priority.ALWAYS);
+
+            button.setText(buttonText);
+            System.out.println(this);
+            button.setOnAction(event -> this.getChildren().removeAll(this.getChildren()));
+
+            this.getChildren().addAll(label, button);
+        }
+
+    }
+
+    @FXML
+    private void initialize() {
+        cheeseSize.setItems(pizzaSizeList);
+        pepSize.setItems(pizzaSizeList);
+        supSize.setItems(pizzaSizeList);
+        pepsiSize.setItems(sodaSizeList);
+        cokeSize.setItems(sodaSizeList);
+        dewSize.setItems(sodaSizeList);
+        BorderPane layout = new BorderPane();
+        List<HBoxCell> list = new ArrayList<>();
+        ListView<HBoxCell> listView = new ListView<HBoxCell>();
+
+        listView.setItems(myObservableList);
+        layout.setCenter(listView);
+    }
+    @FXML
+    protected void cheeseOnMouseClicked() {
+        list.add(new HBoxCell(cheeseSize.getValue() +" Cheese Pizza","Remove"));
+        listView.setItems(myObservableList);
+        listView.refresh();
+    }
+    @FXML
+    protected void pepOnMouseClicked() {
+        list.add(new HBoxCell(pepSize.getValue() +" Pepperoni Pizza","Remove"));
+        listView.setItems(myObservableList);
+        listView.refresh();
+    }
+    @FXML
+    protected void supOnMouseClicked() {
+        list.add(new HBoxCell(supSize.getValue() +" Supreme Pizza","Remove"));
+        listView.setItems(myObservableList);
+        listView.refresh();
+    }
+    @FXML
+    protected void pepsiOnMouseClicked() {
+        list.add(new HBoxCell(pepsiSize.getValue() +" Pepsi","Remove"));
+        listView.setItems(myObservableList);
+        listView.refresh();
+    }
+    @FXML
+    protected void cokeOnMouseClicked() {
+        list.add(new HBoxCell(cokeSize.getValue() +" Coca-Cola","Remove"));
+        listView.setItems(myObservableList);
+        listView.refresh();
+    }
+    @FXML
+    protected void dewOnMouseClicked() {
+        list.add(new HBoxCell(dewSize.getValue() +" Mountain Dew","Remove"));
+        listView.setItems(myObservableList);
+        listView.refresh();
+    }
+
+
+}
