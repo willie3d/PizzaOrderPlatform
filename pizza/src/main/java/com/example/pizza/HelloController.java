6628102b55526bbfcfa2db6262e489cdf6e81566
willie3d <www.willdoyle10@gmail.com>
2021-12-02 12:20:19 -0500

    Remove from receipt implemented

--- pizza/src/main/java/com/example/pizza/HelloController.java
+++ pizza/src/main/java/com/example/pizza/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.pizza;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -25,7 +30,16 @@ public class HelloController {
     ObservableList<HBoxCell> myObservableList = FXCollections.observableList(list);
     @FXML
     private Label welcomeText;
-
+    @FXML
+    private Label recieptTotalLabel;
+    @FXML
+    public static double totalPrice;
+    private double getTotalPrice(){
+        return totalPrice;
+    }
+    private void setTotalPrice(double price){
+        totalPrice = price;
+    }
     @FXML
     private ComboBox cheeseSize;
     @FXML
@@ -69,30 +83,47 @@ public class HelloController {
     @FXML
     ListView<HBoxCell> listView = new ListView<HBoxCell>();
 
-
+    @FXML
+    protected static void removeButton(double amount){
+        totalPrice = totalPrice - amount;
+    }
+    @FXML
+    protected void refresh(){
+        Timeline fiveSecondsWonder = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                recieptTotalLabel.setText(String.valueOf(totalPrice));
+            }
+        }));
+        fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
+        fiveSecondsWonder.play();
+    }
 
 
 
     public static class HBoxCell extends HBox {
         Label label = new Label();
         Button button = new Button();
+        double cost;
+        private double getCost(){
+            return cost;
+        }
 
-        HBoxCell(String labelText, String buttonText) {
+        HBoxCell(String labelText, String buttonText, double price) {
             super();
-            int cost = 0;
             label.setText(labelText);
             label.setMaxWidth(Double.MAX_VALUE);
             HBox.setHgrow(label, Priority.ALWAYS);
+            cost = price;
 
             button.setText(buttonText);
             System.out.println(this);
             button.setOnAction(event -> this.getChildren().removeAll(this.getChildren()));
-
+            button.setOnMousePressed(event -> removeButton(price));
             this.getChildren().addAll(label, button);
         }
 
     }
-
     @FXML
     private void initialize() {
         cheeseSize.setItems(pizzaSizeList);
@@ -107,42 +138,115 @@ public class HelloController {
 
         listView.setItems(myObservableList);
         layout.setCenter(listView);
+        refresh();
     }
     @FXML
-    protected void cheeseOnMouseClicked() { //test
-        list.add(new HBoxCell(cheeseSize.getValue() +" Cheese Pizza","Remove"));
+    protected void cheeseOnMouseClicked() {
+        double thisPrice = 0;
+        if(cheeseSize.getValue().toString() == "Small"){
+            thisPrice = 10.99;
+        } else if(cheeseSize.getValue().toString() == "Medium"){
+            thisPrice = 12.99;
+        } else if(cheeseSize.getValue().toString() == "Large"){
+            thisPrice = 14.99;
+        } else if(cheeseSize.getValue().toString() == "Extra Large"){
+            thisPrice = 15.99;
+        }
+        HBoxCell cheesePizzaCell = new HBoxCell(String.valueOf(thisPrice) + "   " + cheeseSize.getValue() +" Cheese Pizza","Remove",thisPrice);
+        list.add(cheesePizzaCell);
         listView.setItems(myObservableList);
         listView.refresh();
+        totalPrice = totalPrice + cheesePizzaCell.getCost();
+        recieptTotalLabel.setText(String.valueOf(totalPrice));
     }
     @FXML
     protected void pepOnMouseClicked() {
-        list.add(new HBoxCell(pepSize.getValue() +" Pepperoni Pizza","Remove"));
+        double thisPrice = 0;
+        if(pepSize.getValue().toString() == "Small"){
+            thisPrice = 10.99;
+        } else if(pepSize.getValue().toString() == "Medium"){
+            thisPrice = 12.99;
+        } else if(pepSize.getValue().toString() == "Large"){
+            thisPrice = 14.99;
+        } else if(pepSize.getValue().toString() == "Extra Large"){
+            thisPrice = 15.99;
+        }
+        HBoxCell pepPizzaCell = new HBoxCell(String.valueOf(thisPrice) + "   " + pepSize.getValue() +" Pepperoni Pizza","Remove",thisPrice);
+        list.add(pepPizzaCell);
         listView.setItems(myObservableList);
         listView.refresh();
+        totalPrice = totalPrice + pepPizzaCell.getCost();
+        recieptTotalLabel.setText(String.valueOf(totalPrice));
     }
     @FXML
     protected void supOnMouseClicked() {
-        list.add(new HBoxCell(supSize.getValue() +" Supreme Pizza","Remove"));
+        double thisPrice = 0;
+        if(supSize.getValue().toString() == "Small"){
+            thisPrice = 10.99;
+        } else if(supSize.getValue().toString() == "Medium"){
+            thisPrice = 12.99;
+        } else if(supSize.getValue().toString() == "Large"){
+            thisPrice = 14.99;
+        } else if(supSize.getValue().toString() == "Extra Large"){
+            thisPrice = 15.99;
+        }
+        HBoxCell supPizzaCell = new HBoxCell(String.valueOf(thisPrice) + "   " + supSize.getValue() +" Supreme Pizza","Remove",thisPrice);
+        list.add(supPizzaCell);
         listView.setItems(myObservableList);
         listView.refresh();
+        totalPrice = totalPrice + supPizzaCell.getCost();
+        recieptTotalLabel.setText(String.valueOf(totalPrice));
     }
     @FXML
     protected void pepsiOnMouseClicked() {
-        list.add(new HBoxCell(pepsiSize.getValue() +" Pepsi","Remove"));
+        double thisPrice = 0;
+        if(pepsiSize.getValue().toString() == "Can"){
+            thisPrice = 0.99;
+        } else if(pepsiSize.getValue().toString() == "Bottle"){
+            thisPrice = 1.49;
+        } else if(pepsiSize.getValue().toString() == "2-Liter"){
+            thisPrice = 1.99;
+        }
+        HBoxCell pepsiSodaCell = new HBoxCell(String.valueOf(thisPrice) + "   " + pepsiSize.getValue() +" Pepsi","Remove",thisPrice);
+        list.add(pepsiSodaCell);
         listView.setItems(myObservableList);
         listView.refresh();
+        totalPrice = totalPrice + pepsiSodaCell.getCost();
+        recieptTotalLabel.setText(String.valueOf(totalPrice));
     }
     @FXML
     protected void cokeOnMouseClicked() {
-        list.add(new HBoxCell(cokeSize.getValue() +" Coca-Cola","Remove"));
+        double thisPrice = 0;
+        if(cokeSize.getValue().toString() == "Can"){
+            thisPrice = 0.99;
+        } else if(cokeSize.getValue().toString() == "Bottle"){
+            thisPrice = 1.49;
+        } else if(cokeSize.getValue().toString() == "2-Liter"){
+            thisPrice = 1.99;
+        }
+        HBoxCell cokeSodaCell = new HBoxCell(String.valueOf(thisPrice) + "   " + cokeSize.getValue() +" Coca-Cola","Remove",thisPrice);
+        list.add(cokeSodaCell);
         listView.setItems(myObservableList);
         listView.refresh();
+        totalPrice = totalPrice + cokeSodaCell.getCost();
+        recieptTotalLabel.setText(String.valueOf(totalPrice));
     }
     @FXML
     protected void dewOnMouseClicked() {
-        list.add(new HBoxCell(dewSize.getValue() +" Mountain Dew","Remove"));
+        double thisPrice = 0;
+        if(dewSize.getValue().toString() == "Can"){
+            thisPrice = 0.99;
+        } else if(dewSize.getValue().toString() == "Bottle"){
+            thisPrice = 1.49;
+        } else if(dewSize.getValue().toString() == "2-Liter"){
+            thisPrice = 1.99;
+        }
+        HBoxCell dewSodaCell = new HBoxCell(String.valueOf(thisPrice) + "   " + dewSize.getValue() +" Moutain Dew","Remove",thisPrice);
+        list.add(dewSodaCell);
         listView.setItems(myObservableList);
         listView.refresh();
+        totalPrice = totalPrice + dewSodaCell.getCost();
+        recieptTotalLabel.setText(String.valueOf(totalPrice));
     }
     @FXML
     public void toLoginButton() throws IOException {
